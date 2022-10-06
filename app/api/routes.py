9d035055461ef65ad3c480f485f8887b371ab9d3
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-10-06 11:30:37 +0100

    setup login function

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request, current_app
-import os
 from pymongo import MongoClient
-import certifi
 from bson import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.api import api
+from functools import wraps
+import os
+import certifi
+import jwt
 
 mongopass = os.getenv('mongopass')
 ca = certifi.where()
@@ -14,6 +16,8 @@ db = client.wedding
 
 app = Flask(__name__)
 
+
+# needed functions
 def doc2json(document):
 	json = {}
 	for k, v in document.items():
@@ -23,6 +27,34 @@ def doc2json(document):
 			json[k] = v
 	return json
 
+def login_required(f):
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		guest_id = kwargs['guest_id']
+		auth = request.headers.get('Authorization')
+
+		try:
+			token = auth.split(' ')[1]
+			payload = jwt.decode(token, current_app.config.get('SECRET_KEY'), algorithms=['HS256'])
+			guest = db.guests.find_one({'_id': ObjectId(guest_id)})
+
+			assert guest
+			assert guest_id == payload['_id']
+			assert guest['token'] == token
+		except:
+			return jsonify({'error': 'Invalid credentials.'}), 401
+
+		return f(*args, **kwargs)
+
+	return wrapper
+
+
+
+
+
+
+
+# Routes
 @api.route('/', methods=['GET'])
 def home():
     return 'Welcome to the wedding backend'
